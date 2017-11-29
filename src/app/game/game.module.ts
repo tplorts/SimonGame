66dabd1ca4df169265abf7870ff25c658393687e
Ted Lorts <tplorts@gmail.com>
2017-11-29 11:32:15 -0800

    generated the game component

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GameComponent } from './game.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [GameComponent]
 })
 export class GameModule { }
