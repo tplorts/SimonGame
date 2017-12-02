1bba48f1a2a9bec6905eb1e2e6eb9cf0f9d26d4d
Ted Lorts <tplorts@gmail.com>
2017-12-02 13:33:56 -0800

    added test for random index generation

--- src/app/game/game-engine.service.spec.ts
+++ src/app/game/game-engine.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed, inject } from '@angular/core/testing'
 
 import { GameModule } from './game.module'
 import { GameEngineService } from './game-engine.service'
+import { GameConfigService } from './game-config.service'
 
 
 function makeSequence (engine: GameEngineService, length: number) {
@@ -27,6 +28,19 @@ describe('GameEngineService', () => {
   it('should begin with an empty sequence', inject([GameEngineService], (engine: GameEngineService) => {
     expect(engine.sequence.length).toBe(0)
   }))
+
+  const N = 100
+  it(`should generate ${N} random Simon indices within [0, number-of-simons)`, inject(
+    [GameEngineService, GameConfigService],
+    (engine: GameEngineService, config: GameConfigService) => {
+      const UpperBound = config.numberOfSimons
+      const indicies = new Array(N)
+      for (let i = 0; i < N; i++) {
+        indicies[i] = engine.randomSimonIndex()
+      }
+      expect(indicies.every(value => 0 <= value && value < UpperBound)).toBe(true)
+    },
+  ))
 
   const TestLength = 10
   it(`should add ${TestLength} Simons to the sequence`, inject([GameEngineService], (engine: GameEngineService) => {
